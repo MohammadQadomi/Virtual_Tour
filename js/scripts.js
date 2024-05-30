d74e682148991b88bd434b8a504afd4da82f1d08
Mohammad Qadomi <mohammad.qadomi@paltel.ps>
2024-05-30 14:18:31 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-import * as THREE from "three";
+//import * as THREE from "three";
+import * as THREE from "../../node_modules/three/src/Three.js";
+//import {OrbitControls} from "../../node_modules/three/examples/jsm/controls/OrbitControls.js";
 
 const renderer = new THREE.WebGLRenderer();
 
